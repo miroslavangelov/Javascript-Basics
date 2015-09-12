f9bc541eeafdaec1af688a0f75b6e8754e4d72a0
Miroslav Angelov <angelov_90@abv.bg>
2015-09-13 00:03:06 +0300

    First commit

---
+++ strings/6getTextBetweenTags.js
@@ -0,0 +1,6 @@
+﻿	/*Write a function that extracts the content of a html page given as text. The function should return anything that is in a tag, without the tags:
+*/
+	
+	var getHtml = document.documentElement.innerHTML,
+		repl = getHtml.replace(/<(.*?)>/g, "");
+		console.log(repl);
